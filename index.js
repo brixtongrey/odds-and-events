b27c1f19fbde77a5734f87422859fb5b940293cd
Brixtongrey <brixtonaruda@gmail.com>
2025-08-19 20:03:31 -0600

    feat: initial commit with state

---
+++ index.js
@@ -0,0 +1,13 @@
+// ------- STATE ----------
+const number = [];
+const bank = [];
+const odds = [];
+const evens = [];
+
+/* TODO:
+- Create a method to add a number to the bank
+- Create a method to sort one
+- Create a method to sort all
+/*
+
+
